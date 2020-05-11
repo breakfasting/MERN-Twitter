59898ca74794c97680e85569c95fe76f0bff1c45
breakfasting <breakfast.ing@gmail.com>
2020-05-11 00:02:16 -0700

    postbuild commands for heroku push

--- app.js
+++ app.js
@@ -9,6 +9,14 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 const db = require('./config/keys').mongoURI;
+const path = require('path');
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  });
+}
 
 mongoose
   .connect(db, { useNewUrlParser: true })
